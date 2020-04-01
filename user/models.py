58f578fa5eba9406fd14807b7e9e7020ea51983f
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-01 18:43:21 -0400

    Worked on custom user model implementation

--- user/models.py
+++ user/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-	pass
+
+    username = models.CharField(max_length-40, unique=True)
+    email = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
