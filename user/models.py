2595cf5115e1d9d54c603dd945e23064c1c3450b
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-02 18:00:30 -0400

    Added AbstractBaseUser

--- user/models.py
+++ user/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
-class User(AbstractUser):
+class User(AbstractBaseUser):
 
-    username = models.CharField(max_length-40, unique=True)
+    username = models.CharField(max_length=40, unique=True)
     email = models.EmailField()
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
+
+    REQUIRED_FIELDS = []
+
+
