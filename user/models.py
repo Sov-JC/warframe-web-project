1395a534b8bcf16667cfc9dd23538a7cfd33aa2a
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-03 16:54:11 -0400

    Finished implementing custom User model and manager.

--- user/models.py
+++ user/models.py
@@ -26,7 +26,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db) #??
         return user
     
-    #???
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
@@ -39,18 +38,62 @@ class UserManager(BaseUserManager):
         
         return self._create_user(email, password, **extra_fields)
 
-# Create your models here.
+    def create_staffuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
 
     username = models.CharField(max_length=40, unique=True)
-    email = models.EmailField()
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False) # admin, not a super-user
+    admin = models.BooleanField(default=False) # a superuser
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
 
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [] #used in interactive only 
 
     objects = UserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+    
+    @property
+    def is_active(self):
+        return self.active
+
+    def __str__(self):
+        return self.email
 
 
 
