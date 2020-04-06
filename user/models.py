1316150020a8d5cfb4d61da2b9acc6de1aa1bb77
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-06 19:19:04 -0400

    Continued working on custom User model

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 #from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin 
 
 '''
 imports used in code snippet
@@ -22,17 +23,21 @@ class UserManager(BaseUserManager):
         
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields) #???
-        user.set_password() #???
+        user.set_password(password) #???
         user.save(using=self._db) #??
         return user
     
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
         
@@ -47,18 +52,16 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
-class User(AbstractBaseUser):
-
-    username = models.CharField(max_length=40, unique=True)
+class User(AbstractBaseUser, PermissionsMixin):
+    #username = models.CharField(max_length=40, unique=True)
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True,
     )
-    active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False) # admin, not a super-user
-    admin = models.BooleanField(default=False) # a superuser
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) # admin, not a super-user
+    #admin = models.BooleanField(default=False) # a superuser
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
@@ -73,24 +76,34 @@ class User(AbstractBaseUser):
     def get_short_name(self):
         return self.email
 
+
+    #Just use the default one from PermissionsMixin
+    '''
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
         return True
+    '''
 
     def has_module_perms(self, app_label):
         return True
-
+    
+    '''
     @property
     def is_staff(self):
         return self.staff
+    '''
 
+    '''
     @property
     def is_admin(self):
         return self.admin
+    '''
     
+    '''
     @property
     def is_active(self):
         return self.active
+    '''
 
     def __str__(self):
         return self.email
