6fcfb73e5193a242211d9d98875f2c96bb33d846
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-19 18:56:39 -0400

    Login view now logs user in on success

--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import LoginForm
@@ -6,11 +8,26 @@ from .forms import LoginForm
 def index(request):
 	return HttpResponse("Hello, world. You're at the users index.")
 
-def login(request):
+def login_view(request):
 
 	if request.method == 'POST':
 		form = LoginForm(request.POST)
+
+		
+		
 		if form.is_valid():
+			email = form.cleaned_data["email_address"]
+			password = form.cleaned_data["password"]
+
+			user = authenticate(username=email, password=password)
+
+			if user is not None:
+				login(request, user)
+				return HttpResponse("User is not None. Authentication occured but not logged in.")
+			else:
+				return HttpResponse("User authentication failed")
+
+
 			return HttpResponse("Form is valid")
 		else:
 			return HttpResponse("Form invalid!")
@@ -19,7 +36,7 @@ def login(request):
 	
 	context = {'form': form}
 
-	return render(request, 'login.html', context)
+	return render(request, 'user/login.html', context)
 
 def logout(request):
 	return HttpResponse("Not implemented yet")
