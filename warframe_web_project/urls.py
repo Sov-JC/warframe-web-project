62d813217213391929368c01241ac44dcfa3039d
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-22 20:23:08 -0400

    Added url path to the home app for URL's with only a domain name (prefixed by a protocol) but with no path. This was done since the home app will contain the homepage logic.

--- warframe_web_project/urls.py
+++ warframe_web_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('', include('home.urls')),
 ]
