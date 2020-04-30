b38428302c7fbd113acff754af546c130cd8b803
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-04-30 00:16:00 -0400

    Home app's url conf correctly points to appropriate template relative path

--- home/views.py
+++ home/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the home index.")
+	return render(request, 'home/index.html', context=None)
+	
