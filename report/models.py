f547e54faddd9003a5a8ab832f8477b9d2bc615d
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-03 21:10:07 -0400

    Added report app along with model placeholder in its modely.py file.

---
+++ report/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+class ReportCase(models.Model):
+	pass
+
+class ReportState(models.Model):
+	pass
+
+class ImageProof(models.Model):
+	pass
+
+class VideoProof(models.Model):
+	pass
+
