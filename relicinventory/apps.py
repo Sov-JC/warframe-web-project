e619d8a20875bff270584d70d048f199574b4194
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-03 21:09:11 -0400

    Added relicinventory app with model placeholder in its models.py

---
+++ relicinventory/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RelicinventoryConfig(AppConfig):
+    name = 'relicinventory'
---
+++ relicinventory/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+class Relic(models.Model):
+	pass
+
+class OwnedRelic(models.Model):
+	pass
+
