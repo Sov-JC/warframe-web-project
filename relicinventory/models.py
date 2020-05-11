374a30870f605dd881b969171ba5c889490f592b
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-11 01:35:27 -0400

    Implemented the Relic and Owned user model for the relicinventory app

--- relicinventory/models.py
+++ relicinventory/models.py
@@ -1,10 +1,40 @@
+from django.conf import settings
 from django.db import models
 from user.models import User
+from .managers import OwnedRelicManager
 
 # Create your models here.
 class Relic(models.Model):
-	pass
+    relic_id = models.AutoField(primary_key=True)
+    relic_name = models.CharField(max_length=32, unique=True, default=None)
+    wiki_url = models.CharField(max_length=512, default=None)
 
 class OwnedRelic(models.Model):
-	pass
+	owned_relic_id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.PROTECT,
+		default=None # validation should make sure default doesn't execute
+	)
+	relic_id = models.ForeignKey(
+		'Relic',
+		on_delete=models.PROTECT,
+		default=None # validation should make sure default doesn't execute
+	)
+
+	objects = OwnedRelicManager()
+
+	class Meta:
+		db_table = "owned_relic"
+		constraints = [
+			models.UniqueConstraint(
+				fields=['user_id','relic_id'],
+				name='unique_relic_owned'
+			)
+		]
+
+	
+
+
+	
 
