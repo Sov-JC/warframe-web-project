2d8e08cc45738874a513d91ae07aa7f489593305
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-15 09:58:59 -0400

    Modified chat app
    Added the managers.py file, along with very minimal managers.
    
    models.py
    	- Added default=None to several fields where a default was necessary.
    	- initialized the Chat model and ChatMessage model with their appropriate manager
    	  references by the objects property.

---
+++ chat/managers.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class ChatManager(models.Manager):
+	pass
+
+class ChatMessageManager(models.Manager):
+	pass
--- chat/models.py
+++ chat/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from user.models import User
 from user.models import WarframeAccount
 
+from .managers import *
+
 # Create your models here.
 class Chat(models.Model):
 	chat_id = models.AutoField(primary_key=True)
@@ -11,16 +13,20 @@ class Chat(models.Model):
 	person_one_warframe_account_id = models.ForeignKey(
 		WarframeAccount,
 		on_delete=models.PROTECT,
-		related_name = "person_one_wf_account_id"
+		related_name = "person_one_wf_account_id", #this is prob incorrect - might need to name it something like 'chat_person_one'
+		default=None,
 	)
 	person_two_warframe_account_id = models.ForeignKey(
 		WarframeAccount,
 		on_delete=models.PROTECT,
-		related_name = "person_two_wf_account_id"
+		related_name = "person_two_wf_account_id", #this is prob incorrect
+		default=None,
 	)
 	
 	person_one_still_in_chat = models.BooleanField(default=True)
 	person_two_still_in_chat = models.BooleanField(default=True)
+
+	objects = ChatManager()
 	
 	class Meta:
 		constraints = [
@@ -38,14 +44,18 @@ class ChatMessage(models.Model):
 	chat_message_id = models.AutoField(primary_key=True)
 	chat_id = models.ForeignKey(
 		Chat,
-		on_delete = models.CASCADE
+		on_delete = models.CASCADE,
+		default=None
 	)
 	formUserID = models.ForeignKey(
 		settings.AUTH_USER_MODEL,
-		on_delete = models.PROTECT
+		on_delete = models.PROTECT,
+		default=None
 	)
-	message = models.CharField(max_length=45)
+	message = models.CharField(max_length=45, default=None)
 	message_created = models.DateTimeField(auto_now_add=True, blank=True)
+
+	objects = ChatMessageManager()
 
 	class Meta:
 		db_table = 'chat_chat_message'
