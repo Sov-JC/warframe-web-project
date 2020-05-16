ef36deafba9cab100f349ed2f189d7bfeb95679c
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-16 12:48:26 -0400

    Added managers to the report app. Made the model's 'object' property reference the appropriate model.

---
+++ report/managers.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class ReportCaseManager(models.Manager):
+	pass
+
+class ReportStateManager(models.Manager):
+	pass
+
+class ImageProofManager(models.Manager):
+	pass
+
+class VideoProofManager(models.Manager):
+	pass
--- report/models.py
+++ report/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from user.models import User
+from .managers import *
 
 # Create your models here.
 class ReportCase(models.Model):
+	objects = ReportCaseManager()
 	pass
 
 class ReportState(models.Model):
+	objects = ReportStateManager()
 	pass
 
 class ImageProof(models.Model):
+	objects = ImageProofManager()
 	pass
 
 class VideoProof(models.Model):
+	objects = VideoProofManager()
 	pass
 
