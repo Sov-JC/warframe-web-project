e2f57f705f7b003d93794156299122e56ec9d60a
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-05-27 21:26:06 -0400

    Added decorator for checking if a user's email has been verified.

---
+++ user/decorators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import PermissionDenied
+from .models import User
+
+def email_validated_user(function):
+	def wrap(request, *args, **kwargs):
+		user = User.objects.get(pk=kwargs['email'])
+
+		if User.email_verified == True:
+			return function(request, *args, **kwargs)
+		else:
+			raise Exception("User must confirm their email address before accessing this view.")
+
+	return wrap
+		
+
+
+	
