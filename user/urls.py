4db8198b1f9311044488fc2e1192a6ec7cf9f505
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-06-10 06:40:01 -0400

    Added news routes for the the warframe linking and can't-log-in page.

--- user/urls.py
+++ user/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout/', views.log_user_out, name='logout'),
     path('profile/', views.profile, name = 'profile'),
     path('register/', views.register, name = 'register'),
-    path('link-wf-acc', views.link_warframe_account, name='link_wf_acc')
+    path('link-wf-acc', views.link_warframe_account, name='link_wf_acc'),
+    path('cant-log-in', views.cant_log_in, name='cant_log_in'),
 ]
