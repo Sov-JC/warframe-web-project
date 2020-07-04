090d3546574b34a4bae8bccf2a4f10626b4dc530
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-07-04 12:59:53 -0400

    Added a docstring template that can be used for copy pasting.

---
+++ docstring-example.py
@@ -0,0 +1,10 @@
+'''Deletes the PrivateInteractionClosed
+
+:param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+:type [ParamName]: [ParamType](, optional)
+...
+:raises [ErrorType]: [ErrorDescription]
+...
+:return: [ReturnDescription]
+:rtype: [ReturnType]
+'''
