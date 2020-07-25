725184bf44966208e0b94a85c25b005e8d58af1e
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-07-24 20:11:44 -0400

    Added front-end logic for the inventory page.

--- static/relicinventory/my-inventory.js
+++ static/relicinventory/my-inventory.js
@@ -1,54 +1,129 @@
+// Show type constants
+var SHOW_CHECKED = "Show Checked"
+var SHOW_ALL = "Show All"
+var SHOW_UNCHECKED = "Show Unchecked"
 
-/*
-Represents a relic that is either checked (owned) in its corresponding
-checkbox element or unchecked (not owned)
-*/
-class RelicCheckState {
-	relicId;
-	isChecked;
-	constructor(relicId, isChecked){
-		this.relic_id=relicId
-		this.isChecked=isChecked
-	}
+
+function show(showTypeConstant){
+	/*Filter the list of relics by the show type constants.
+	For example, show(SHOW_CHECKED) displays all the relics that are
+	checked.
+	*/
+	var RELIC_CHECKBOX_DEFAULT_DISPLAY = "block"
+	
+
+	$(".relics-container .relic").filter(
+		function(index, element){
+			//The relic's corresponding checkbox
+			elCheckbox = element.firstElementChild
+
+			var checked = elCheckbox.checked
+
+			console.log("checked[" + index + "]: " + checked)
+
+			if(showTypeConstant === SHOW_CHECKED)
+				if(checked === true)
+					element.style.display = RELIC_CHECKBOX_DEFAULT_DISPLAY
+				else 
+					element.style.display = "none"
+			else if(showTypeConstant === SHOW_UNCHECKED)
+				if(checked === false){
+					element.style.display = RELIC_CHECKBOX_DEFAULT_DISPLAY
+				}else
+					element.style.display = "none"
+			else if(showTypeConstant === SHOW_ALL)
+				element.style.display = RELIC_CHECKBOX_DEFAULT_DISPLAY
+		}
+	)
 }
 
-/*
-Returns a list of RelicCheckStates. Each element
-representing a relic that is either owned or not owned
-by the user based on 'My Inventory' client-side state.
-*/
-function getRelicCheckStates(){
-	$relicCheckBoxes = $('.relic input:checkbox')
+function showUnchecked(event){
+	show(SHOW_UNCHECKED)
+}
 
-	var relicCheckStates = []
+function showAll(event){
+	show(SHOW_ALL)
+}
 
-	$relicCheckBoxes.each(
+function showChecked(event){
+	show(SHOW_CHECKED)
+}
+
+function getCheckedRelicIds(){
+	//console.log("call::getCheckedRelicIds()")
+	$relics = $(".relic input:checkbox")
+
+	//console.log("$relics length is: " + $relics.length)
+	$checkedRelics = $relics.filter(
 		function(index, value){
-			relicCheckState = new RelicCheckState(value.id, value.checked)
-			relicCheckStates.push(relicCheckState)
+			// console.log("index: " + index)
+			// if(value.checked){
+			// 	console.log(document.getElementById(value.id).nextElementSibling.textContent + " is checked")
+			// }else{
+			// 	console.log(document.getElementById(value.id).nextElementSibling.textContent + " is NOT checked")
+			// }
+			return value.checked
 		}
 	)
 
-	return relicCheckStates
+	var checkedRelicIds = []
+	// console.log("$checkedRelics length is: " + $checkedRelics.length)
+	$.each($checkedRelics, function(index, value){
+		checkedRelicIds.push(parseInt(value.id))
+	});
+
+	console.log("checked relic ids::: ")
+	console.log(checkedRelicIds)
+
+	console.log("CheckedRelicIds -> " + JSON.stringify(checkedRelicIds))
+
+	return checkedRelicIds
 }
 
 $('#save button').on('click', function(event){
-	relicCheckStates = getRelicCheckStates()
-	var jsonStr = JSON.stringify(relicCheckStates)
-	console.log("[jsonStr:"+jsonStr+"]")
-
+	//relicCheckStates = getRelicCheckStates()
+	//var jsonStr = JSON.stringify(relicCheckStates)
+	//console.log("[jsonStr:"+jsonStr+"]")
+	var checkedRelicIds = getCheckedRelicIds()
 	var saveUrl = $(".relics-container").attr('data-save-url')
-	console.log("$saveUrl = " + saveUrl)
+	//console.log("saveUrl = " + saveUrl)
+	console.log("checkedRelicIds: ") 
+	console.log(checkedRelicIds)
+	console.log("typeof(checkedRelicIds)")
+	console.log(typeof(checkedRelicIds))
+	//JSON.stringify(checkedRelicIds)
+	console.log("type of [1,2,3] is: " + typeof([1,2,3]))
 	$.ajax({
 		url: saveUrl,
-		data: {'relicCheckStates': relicCheckStates},
+		data: JSON.stringify(checkedRelicIds),
 		type: 'PUT',
 		dataType: 'json',
+		timeout: 2000,
 		success: function(data) {
-			console.log("successful ajax call")
+			console.log("Successfully updated inventory.")
+			//console.log("error: " + data["error"])
+			//console.log("success: " + data["success"])
 		},
 		error: function(data){
-			console.log("ajax call failed")
+			console.log("error: " + data["error"])
+			//console.log("success: " + data["success"])
+			console.log("Attempts to update inventory failed.")
 		}
 	});
+});
+
+// checked, all, and unchecked links
+$('#all-relics').on('click', function(event){
+	showAll()
+	event.preventDefault()
+});
+
+$('#checked-relics').on('click', function(event){
+	console.log("checked clicked")
+	showChecked()
+	event.preventDefault()
+});
+$('#unchecked-relics').on('click', function(event){
+	showUnchecked()
+	event.preventDefault()
 });
