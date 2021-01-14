d39fbb4953d424aa17c9789f773bfa13ff9c0b30
Jorge Costafreda <jorge.costafreda@yahoo.com>
2021-01-14 18:40:04 -0500

    relicinventory/managers.py
    Added two manager functions
    	- get_crowdedness_overview_data function
    		- Added function to generate an overview for the find-player
    		view. Used for generating a summary of the users online
    		with particular relics in their inventory.
    	- get_relic_ids_of_wfa_owned_relics function.

--- relicinventory/managers.py
+++ relicinventory/managers.py
@@ -1,9 +1,110 @@
 from django.db import models
 from django.apps import apps
+from django.db.models import Count
+
+from user.models import UserStatus
 
 class RelicManager(models.Manager):
-	pass
-	
+	#Not tested yet
+	def get_crowdedness_overview_data(self, gaming_platform, linked_wfa_user=None):
+		'''Returns a list of dictionaries. Each dictionary with keys "relic_id", "relic_id__name",
+		"count", and (if @user is not None and linked with a warframe account) "owned_relic". Each element
+		represents a relic id, along with its name, whether @linked_wfa_user's wfa has the relic in 
+		their inventory (if the @linked_wfa_user is not None), and the total numbers of online (and linked) users
+		that own the correponding relic on the @gaming_platform gaming platform.
+
+		Example: 
+
+		(If user is None): 
+		[{'relic_id' : '23', 'relic_id__relic_name':'Axi S3', 'count':'56'}, ...]
+
+		(If user is not None with a linked warframe account):
+		[{'relic_id' : '23', 'relic_id__relic_name':'Axi S3', 'count':'56', 'owned_relic': True}, ...]
+
+		:param gaming_platform:
+		:type gaming_platform:
+		:param user: The user whose warframe account's inventory should be searched for to determine
+		if he has the corresponding relic in the queryset of Relic objects in their inventory.
+		:type user: User
+		...
+		:return: Returns a list of dictionaries. Each dictionary contains the following keys:
+		relic_id, relic_id__relic_name, count, and, if @user is not none, owned_relic.
+		:rtype: List of dictionaries
+		'''
+		print("@@@linked_wfa_user is: ", linked_wfa_user)
+		if linked_wfa_user != None:
+			if linked_wfa_user.is_wfa_linked is False:
+				raise ValueError("linked_wfa_user must have a linked Warframe account if linked_wfa_user is not None")
+
+		owned_relic_m = apps.get_model('relicinventory', 'OwnedRelic')
+		user_status_m = apps.get_model('user', 'UserStatus')
+		relic_m = apps.get_model('relicinventory', 'Relic')
+		
+		online_user_status = user_status_m.objects.get(user_status_name=user_status_m.ONLINE)
+
+		query_set = owned_relic_m.objects.filter(
+			warframe_account_id__user__user_status_id=online_user_status,
+			warframe_account_id__gaming_platform_id=gaming_platform.pk
+		).select_related(
+			'warframe_account_id__user_id__user_status_id',
+			'relic',
+		).values(
+			'relic_id',
+			'relic_id__relic_name'
+		).annotate(
+			count=Count("relic_id")
+		).order_by("relic_id")
+
+		#print("qs.query is: ---")
+		#print(query_set.query)
+		#print("query_set is: ---")
+		#print(query_set)
+
+		query_set = list(query_set)
+
+		# All relics that are now in query_set represent those relics that are owned by at least
+		# one online user. The query_set does not contain relics that are not owned by users
+		# that are currently online. Let's add those relics, along with appropriate corresponding,
+		# data, to query_set.
+		all_relics = relic_m.objects.all().values_list('relic_id','relic_name')
+		RELIC_ID = 0
+		RELIC_NAME = 1
+
+		qs_missing_relic_ids = [row["relic_id"] for row in query_set]
+		qs_missing_relic_ids = set(qs_missing_relic_ids)
+
+		for relic in all_relics:
+			if relic[RELIC_ID] not in qs_missing_relic_ids:
+
+				query_set_item = {
+					"relic_id": relic[RELIC_ID],
+					"relic_id__relic_name": relic[RELIC_NAME],
+					"count": 0
+				}
+
+				query_set.append(query_set_item)
+		
+		# If user is not None, then modify the queryset by adding "owned_relic" key to each 
+		# dictionary. owned_relic's value is boolean True if the user owns this relic, False
+		# if not.
+		if linked_wfa_user is not None:
+			user_warframe_account = linked_wfa_user.linked_warframe_account_id
+
+			relic_ids_owned = owned_relic_m.objects.get_relic_ids_of_wfa_owned_relics(user_warframe_account)
+			print("relic_ids_owned: ", relic_ids_owned)
+
+			for row in query_set:
+				if row["relic_id"] in relic_ids_owned:
+					row["owned_relic"] = True
+				else:
+					row["owned_relic"] = False
+
+		
+		
+		return query_set
+
+
+
 class OwnedRelicManager(models.Manager):
 
 	#Tested
@@ -37,6 +138,19 @@ class OwnedRelicManager(models.Manager):
 		relics = relic_m.objects.filter(relic_id__in=wfa_owned_relics)
 
 		return relics
+
+	#Untested
+	def get_relic_ids_of_wfa_owned_relics(self, warframe_account):
+		'''Returns a set of Relic ids that are in @warframe_account's
+		inventory
+		'''
+		owned_relic_m = apps.get_model("relicinventory", "OwnedRelic")
+
+		owned_relics = owned_relic_m.objects.filter(warframe_account_id=warframe_account)
+
+		return set(owned_relics.values_list("relic_id", flat=True))
+
+
 
 	#Tested
 	def clear_wfa_inventory(self, warframe_account):
@@ -96,5 +210,10 @@ class OwnedRelicManager(models.Manager):
 		self.bulk_create(owned_relics)
 
 		return owned_relics
+
+	
+
+	
+	
 
 
