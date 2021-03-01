74541aa53d035d0f73c4f295486eac14a327a5e3
Jorge Costafreda <jorge.costafreda@yahoo.com>
2021-03-01 18:45:32 -0500

    Added py file, as a reference, for common database constants

---
+++ dbinfo/db_constants.py
@@ -0,0 +1,20 @@
+#user_status table
+USER_STATUS_OFFLINE_ID = 1
+USER_STATUS_ONLINE_ID = 2
+
+#gaming_platform table
+PC_ID = 1
+XBOX_ID = 2
+PLAY_STATION_ID = 3
+NINTENDO_SWITCH_ID = 4
+
+
+class UserStatus:
+	OFFLINE_ID = 1
+	ONLINE_ID = 2
+
+class GamingPlatform:
+	PC_ID = 1
+	XBOX_ID = 2
+	PLAY_STATION = 3
+	NINTENDO_SWITCH = 4
